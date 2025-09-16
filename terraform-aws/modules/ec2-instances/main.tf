a3507b5c66a8447977cd45d7d5fddd4e7d5a8dd6
Thiago Vinicius <116599158+Thiago309@users.noreply.github.com>
2025-09-16 12:33:11 -0300

    modules
    Criação de:
    
    - main.tf
    - variables.tf

--- terraform-aws/modules/ec2-instances/main.tf
+++ terraform-aws/modules/ec2-instances/main.tf
@@ -1,4 +1,9 @@
+# Arquivo de entrada - dados do meu provedor (atribuição de valores)
+
 resource "aws_instance" "ec2_instance" {
-  count = var.instance_count
-  
+  count         = var.instance_count
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  subnet_id     = var.subnet_id
+
 }
